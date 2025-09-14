b808741505d0fd5d98cff5e79f1e2cb3d53d7c59
salahudeen abdulkareem <salayo4real@gmail.com>
2025-09-14 21:22:22 +0100

    new update

--- 0x04-TypeScript/README.md
+++
@@ -1,3 +0,0 @@
-# frontend-javascript
-## Creating an interface for a student
-the step by step set up 
---
+++ 0x04-TypeScript/task_3/interface.ts
@@ -0,0 +1,9 @@
+// Create a type RowID
+export type RowID = number;
+
+// Create an interface RowElement
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number; // optional
+}
